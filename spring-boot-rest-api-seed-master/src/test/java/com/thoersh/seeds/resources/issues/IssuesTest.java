ebd9556343122135ebc7001a3072e602af2e57d0
Sudara Fernando <sudara@yahoo.com>
2017-10-21 14:10:42 +1300

    Configured JUnit

---
+++ spring-boot-rest-api-seed-master/src/test/java/com/thoersh/seeds/resources/issues/IssuesTest.java
@@ -0,0 +1,11 @@
+package com.thoersh.seeds.resources.issues;
+
+import org.junit.Test;
+
+public class IssuesTest {
+
+    @Test
+    public void test() {
+        System.out.print("Test works!");
+    }
+}
