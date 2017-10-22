fd80bc5ae0ca2c1d6530728695566ac5fd2b38eb
Sudara Fernando <sudara@yahoo.com>
2017-10-22 16:22:14 +1300

    Store issue details

---
+++ spring-boot-rest-api-seed-master/src/main/java/com/thoersch/seeds/resources/issues/IssuesResource.java
@@ -0,0 +1,56 @@
+package com.thoersch.seeds.resources.issues;
+
+import com.thoersch.seeds.persistence.issues.IssuesRepository;
+import com.thoersch.seeds.representations.issues.Issue;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.inject.Inject;
+import javax.validation.Valid;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Component
+@Path("/issues")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@Transactional
+public class IssuesResource {
+
+    private final IssuesRepository issuesRepository;
+
+    @Inject
+    public IssuesResource(IssuesRepository issuesRepository) {
+        this.issuesRepository = issuesRepository;
+    }
+
+    @GET
+    public List<Issue> getIssues() {
+        return issuesRepository.findAll();
+    }
+
+    @GET
+    @Path("/{id}")
+    public Issue getIssue(@PathParam("id") long id) {
+        Issue issue = issuesRepository.findOne(id);
+
+        if(issue == null) {
+            throw new WebApplicationException(Response.Status.NOT_FOUND);
+        }
+
+        return issue;
+    }
+
+    @POST
+    public Issue saveIssue(@Valid Issue issue) {
+        return issuesRepository.save(issue);
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public void deleteIssue(@PathParam("id") long id) {
+        issuesRepository.delete(id);
+    }
+}
--- spring-boot-rest-api-seed-master/src/test/java/com/thoersh/seeds/resources/issues/IssuesTest.java
+++ spring-boot-rest-api-seed-master/src/test/java/com/thoersh/seeds/resources/issues/IssuesTest.java
@@ -1,11 +1,55 @@
 package com.thoersh.seeds.resources.issues;
 
+import com.thoersch.seeds.persistence.issues.IssuesRepository;
+import com.thoersch.seeds.representations.issues.Issue;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@WebAppConfiguration
 public class IssuesTest {
 
-    @Test
-    public void test() {
-        System.out.print("Test works!");
+   // @Autowired
+    //private IssuesRepository repo;
+
+    private Issue issue;
+
+    @Before
+    public void init() {
+        issue = new Issue();
+        issue.setIssueDetails("Some details");
+        issue.setIssueStatus(Issue.IssueStatus.PENDING);
+        issue.setNumberOfRelatedIssues(3);
     }
+
+    @Test
+    public void testIfSummeryOfIssueIsGenerated() {
+        assertNotNull(issue.getIssueDetails());
+    }
+
+    @Test
+    public void testIfSummeryOfIssueIsNotLengthy() {
+        assertNotNull(issue.getIssueDetails());
+        assertTrue(issue.getIssueDetails().length() > 0);
+        assertTrue(issue.getIssueDetails().length() < 1000);
+    }
+
+    @Test
+    public void testIfNumberOfRelatedPostsAreGreaterThanZero() {
+        assertTrue(issue.getNumberOfRelatedIssues() > 0);
+    }
+
+
 }
