9fbd85a7bbbeb7720b606b1ebcefcd4333f4e003
Lubomira Tzankov <ltza066@aucklanduni.ac.nz>
2017-10-23 13:43:48 +1300

    Add getters and setters

--- spring-boot-rest-api-seed-master/src/main/java/com/thoersch/seeds/representations/forumposts/ForumPost.java
+++ spring-boot-rest-api-seed-master/src/main/java/com/thoersch/seeds/representations/forumposts/ForumPost.java
@@ -46,5 +46,55 @@ public class ForumPost {
         _url = url;
     }
 
+    public Long get_question_id() {
+        return _question_id;
+    }
 
+    public Integer get_forum_details_id() {
+        return _forum_details_id;
+    }
+
+    public void set_forum_details_id(Integer _forum_details_id) {
+        this._forum_details_id = _forum_details_id;
+    }
+
+    public String get_text() {
+        return _text;
+    }
+
+    public void set_text(String _text) {
+        this._text = _text;
+    }
+
+    public Date get_date() {
+        return _date;
+    }
+
+    public void set_date(Date _date) {
+        this._date = _date;
+    }
+
+    public String get_author() {
+        return _author;
+    }
+
+    public void set_author(String _author) {
+        this._author = _author;
+    }
+
+    public String get_content() {
+        return _content;
+    }
+
+    public String get_url() {
+        return _url;
+    }
+
+    public void set_url(String _url) {
+        this._url = _url;
+    }
+
+    public void set_content(String _content) {
+        this._content = _content;
+    }
 }
