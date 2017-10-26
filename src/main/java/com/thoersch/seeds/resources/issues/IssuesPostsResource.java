b2094f1927aafebf4be154e1b2f05c1316f63ddd
Lubomira Tzankov <ltza066@aucklanduni.ac.nz>
2017-10-26 22:02:00 +1300

    Add method stubbing for issues-posts resource

---
+++ src/main/java/com/thoersch/seeds/resources/issues/IssuesPostsResource.java
@@ -0,0 +1,50 @@
+package com.thoersch.seeds.resources.issues;
+
+import com.thoersch.seeds.persistence.forumposts.ForumPostsRepository;
+import com.thoersch.seeds.persistence.issues.IssuesRepository;
+import com.thoersch.seeds.persistence.users.UsersRepository;
+import com.thoersch.seeds.representations.forumposts.ForumPost;
+import com.thoersch.seeds.representations.forumposts.ForumPostCategorizeForm;
+import com.thoersch.seeds.representations.issues.Issue;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.inject.Inject;
+import javax.validation.Valid;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Component
+@Path("/issues/{issueId}/forumposts")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@Transactional
+public class IssuesPostsResource {
+    private final IssuesRepository issuesRepository;
+    private final ForumPostsRepository forumPostsRepository;
+
+    @Inject
+    public IssuesPostsResource(IssuesRepository issuesRepository, ForumPostsRepository forumPostsRepository) {
+        this.issuesRepository = issuesRepository;
+        this. forumPostsRepository = forumPostsRepository;
+    }
+
+    @GET
+    public List<ForumPost> getForumPosts(@PathParam("issueId") long issueId) {
+
+    }
+
+    @POST
+    public ResponseEntity addPostToIssue(@Valid ForumPostCategorizeForm issueCategorizeForm) {
+
+    }
+
+    @POST
+    public ResponseEntity removePostFromIssue(@Valid ForumPostCategorizeForm issueCategorizeForm) {
+
+    }
+
+}
