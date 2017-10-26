11a12be063f1f7195e75499abd827321cb247b1d
Lubomira Tzankov <ltza066@aucklanduni.ac.nz>
2017-10-27 10:26:16 +1300

    Add method for adding forum post to issue

--- src/main/java/com/thoersch/seeds/resources/issues/IssuesPostsResource.java
+++ src/main/java/com/thoersch/seeds/resources/issues/IssuesPostsResource.java
@@ -6,6 +6,8 @@ import com.thoersch.seeds.persistence.users.UsersRepository;
 import com.thoersch.seeds.representations.forumposts.ForumPost;
 import com.thoersch.seeds.representations.forumposts.ForumPostCategorizeForm;
 import com.thoersch.seeds.representations.issues.Issue;
+import com.thoersch.seeds.representations.users.User;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,11 +27,13 @@ import java.util.List;
 public class IssuesPostsResource {
     private final IssuesRepository issuesRepository;
     private final ForumPostsRepository forumPostsRepository;
+    private final UsersRepository usersRepository;
 
     @Inject
-    public IssuesPostsResource(IssuesRepository issuesRepository, ForumPostsRepository forumPostsRepository) {
+    public IssuesPostsResource(IssuesRepository issuesRepository, ForumPostsRepository forumPostsRepository, UsersRepository usersRepository) {
         this.issuesRepository = issuesRepository;
-        this. forumPostsRepository = forumPostsRepository;
+        this.forumPostsRepository = forumPostsRepository;
+        this.usersRepository = usersRepository;
     }
 
     @GET
@@ -44,7 +48,20 @@ public class IssuesPostsResource {
 
     @POST
     public ResponseEntity addPostToIssue(@Valid ForumPostCategorizeForm issueCategorizeForm) {
+        Issue issue = issuesRepository.findOne(issueCategorizeForm.getIssueId());
+        ForumPost toAdd = forumPostsRepository.getOne(issueCategorizeForm.getIssueId());
+        User user = usersRepository.getOne(issueCategorizeForm.getAssignerId());
 
+        //if user is not admin, do not allow
+        if (user.getRole() != User.UserRole.admin) {
+            throw new WebApplicationException(Response.Status.UNAUTHORIZED);
+        }
+
+        issue.addForumPost(toAdd);
+
+        issuesRepository.save(issue);
+
+        return new ResponseEntity<>("UserID: " +user.getId() + " added forum post: "+toAdd.getId() + " to issue: " + issue.getId(), HttpStatus.ACCEPTED);
     }
 
     @POST
